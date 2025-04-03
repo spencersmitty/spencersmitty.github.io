a9d52c09484b3a2a53bb5cc901c101ea068561aa
spencer smith <155290355+spencersmitty@users.noreply.github.com>
2025-04-03 14:21:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("from script file");
+document.addEventListener("DOMContentLoaded", () => {
+  const monke = document.getElementById("monke");
+
+  monke.addEventListener("click", () => {
+    const audio = new Audio('media/ow.wav'); // Path to the "ow.wav" file in the "media" folder
+    audio.play(); // Play sound on click
+  });
+});
